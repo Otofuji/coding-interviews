3474aedc337f8dd24a344c2e9b54e2e313c601f1
Eric Fernando Otofuji Abrantes <ericfoa@al.insper.edu.br>
2023-02-27 21:39:22 -0300

    rotate_image

--- rotate_image/solution.py
+++ rotate_image/solution.py
@@ -11,6 +11,6 @@ class Solution:
         for i in range(n):
             for j in range(n // 2):
                 matrix[i][j], matrix[i][-j - 1] = matrix[i][-j - 1], matrix[i][j]
-    def rotate(self, matrix: List[List[int]]) -> None:
+    def rotate(self, matrix: list[list[int]]) -> None:
         self.transpose(matrix)
         self.reflect(matrix)
